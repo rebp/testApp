eb03297b7d368d1b4351e70908f118acf27e0539
rebp <r.e.bautista.prieto@hotmail.com>
2015-10-11 15:43:25 +0200

    Update ajax_request.php

--- www/ajax_request.php
+++ www/ajax_request.php
@@ -3,7 +3,7 @@
 try {
 	$db_username = "roberbu105_rebp";
 	$db_password = "trebor28";
-	$db = new PDO("mysql:host=localhost;roberbu105_dbname=excellent", $db_username, $db_password);
+	$db = new PDO("mysql:host=localhost;dbname=roberbu105_excellent", $db_username, $db_password);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
 	echo "Error: " . $e->getMessage();
